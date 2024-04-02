1bb5296928e1382b3851f0ddcf1913781078cf2c
Elaine-Ting-Sing-Yi <163113978+Elaine-Ting-Sing-Yi@users.noreply.github.com>
2024-04-02 09:52:50 +0800

    Create bil-elektrik.py 1

---
+++ bil-elektrik.py
@@ -0,0 +1,22 @@
+#Atur cara bagi mengira bayaran bil eletrik
+
+#Pengisytiharan pemboleh ubah dan pemalar
+kadar1=0.218
+kadar2=0.334
+kadar3=0.516
+kadar4=0.546
+
+#input
+e=float(input("Masukkan kadar penggunaan elektrik:"))
+
+#Proses
+if e<=200:
+    bayaran+e*kadar1
+elif e<=300:
+    bayaran =(200*0.218)+((e-200)*kadar2)
+elif e<=600:
+    bayaran =(200*kadar1)+(100*kadar2)+((e-300) * kadar3)
+elif e<=900:
+    bayaran =(200*kadar1)+(100*kadar2)+(300*kadar3)+((e-600)*kadar4)
+
+print(f"bil elktrik=RM{bayaran:.2f}")
